7f7598769c1fab3939ee834daffade92369f4db5
Constantin Ruhdorfer <constantin.ruhdorfer@diconium.com>
2018-06-25 14:08:54 +0200

    build a switch message

--- src/de/dhbw/moviedb_cr/readDdFile.java
+++ src/de/dhbw/moviedb_cr/readDdFile.java
@@ -10,13 +10,33 @@ public class readDdFile {
     public static void readFile() throws IOException {
 
         String in = null;
+        String identifier = null;
 
         try (BufferedReader br = new BufferedReader(new FileReader("movieproject.db.txt"))) {
             while (( in = br.readLine()) != null ) {
                 if (in.contains("New_Entity: ")) {
-                    String workString = in.substring(12);
-                    String[] parts = workString.split(",");
+                    identifier = in.substring(12);
+                } else {
+                    switch (identifier) {
+                        case ("\"actor_id\",\"actor_name\""):
+                            System.out.println("Actor!");
+                            break;
+                        case ("\"movie_id\",\"movie_title\",\"movie_plot\",\"genre_name\",\"movie_released\",\"movie_imdbVotes\",\"movie_imdbRating\""):
+                            System.out.println("Movie!");
+                            break;
+                        case ("\"director_id\",\"director_name\""):
+                            System.out.println("Director!");
+                            break;
+                        case ("\"actor_id\",\"movie_id\""):
+                            break;
+                        case ("\"director_id\",\"movie_id\""):
+                            break;
+                        case ("\"user_name\",\"rating\",\"movie_id\""):
+                            break;
+                        default:
+                            break;
 
+                    }
                 }
             }
             br.close();
